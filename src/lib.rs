fbbbf3f4cc358550f3697c851a784364b9172906
Cameron Dart <cdart@anduril.com>
2020-12-18 22:16:24 -0800

    Identity and Semigroup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,34 @@
+//! # Garfield
+//!
+//! Your guide to Category Theory concepts in Rust using [Generic Associated Types]().
+#![feature(generic_associated_types)]
+#![allow(incomplete_features)]
+
+/// Identity Morphism
+///
+/// For any input, this will give you the same element back.
+pub fn id<T>(t: T) -> T {
+    t
+}
+
+/// Semigroup
+///
+/// The class of semigroups (types with an associative binary operation).
+///     Instances should satisfy the following:
+///
+/// Associativity
+///     x <> (y <> z) = (x <> y) <> z
+pub trait Semigroup {
+    fn concat<T>(&self, other: T) -> T;
+}
+
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn identity() {
         assert_eq!(2 + 2, 4);
     }
 }
