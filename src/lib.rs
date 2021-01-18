a93eb76c57eb78e62e3aa833b4db0ea327a79781
Cameron Dart <cdart@anduril.com>
2021-01-18 15:38:41 -0800

    feat(applicative): finishes Applicative definition
    Implements lift_a2 function and resolves docs issues.

--- src/lib.rs
+++ src/lib.rs
@@ -75,19 +75,52 @@ impl<T, E> Functor for Result<T, E> {
 
 /// A functor with application, providing operations to
 /// - Embed pure expressions using [`pure`]
-/// - Sequence computations and combine their results using [`apply`]
+/// - Sequence computations and combine their results using [`lift_a2`]
 pub trait Applicative: Functor {
     /// Lift a value into a functor
     ///
     /// In Haskell speak this translates to,
     /// pure :: (Functor f) => a -> f a
     fn pure(a: Self::Inner) -> Self::Wrapped<Self::Inner>;
-    // fn apply<B, Self::Wrapped<F: FnOnce()>>(self, fa: Self::Wrapped<Self::Inner>) -> Self::Wrapped<B>;
+    // Run a computation inside a functor.
+    //
+    // In Haskell speak this translates to,
+    // (<*>) :: (Functor f) => f (a -> b) -> f a -> f b -> f b
+    fn lift_a2<B, F, C>(f: F, a: Self::Wrapped<Self::Inner>, b: Self::Wrapped<B>) -> Self::Wrapped<C>
+    where
+        F: Fn(Self::Inner, B) -> C;
 }
 
 impl<A> Applicative for Option<A> {
     fn pure(val: Self::Inner) -> Self::Wrapped<A> {
         Some(val)
+    }
+
+    fn lift_a2<B, F, C>(f: F, fa: Self::Wrapped<Self::Inner>, fb: Self::Wrapped<B>) -> Self::Wrapped<C>
+    where
+         F: Fn(Self::Inner, B) -> C,
+    {
+        match (fa, fb) {
+            (Some(a), Some(b)) => Some(f(a, b)),
+            _ => None,
+        }
+    }
+}
+
+impl<T, E> Applicative for Result<T, E> {
+    fn pure(val: Self::Inner) -> Self::Wrapped<Self::Inner> {
+        Ok(val)
+    }
+
+    fn lift_a2<B, F, C>(f: F, fa: Self::Wrapped<Self::Inner>, fb: Self::Wrapped<B>) -> Self::Wrapped<C>
+    where
+        F: Fn(Self::Inner, B) -> C,
+    {
+        match (fa, fb) {
+            (Ok(a), Ok(b)) => Ok(f(a, b)),
+            (Err(e), _) => Err(e),
+            (_, Err(e)) => Err(e),
+        }
     }
 }
 
@@ -135,5 +168,23 @@ mod tests {
     #[test]
     fn applicative_pure() {
         assert_eq!(<Option<u32> as Applicative>::pure(20), Some(20));
+    }
+
+    #[test]
+    fn applicative_option_some_lift_a2() {
+        let plus = |x: u32, y: u32| x + y;
+        assert_eq!(<Option<u32> as Applicative>::lift_a2(plus, Some(1730), Some(8)), Some(1738));
+    }
+
+    #[test]
+    fn applicative_option_none_lift_a2() {
+        let plus = |x: u32, y: u32| x + y;
+        assert_eq!(<Option<u32> as Applicative>::lift_a2(plus, None, None), None);
+    }
+
+    #[test]
+    fn applicative_option_some_none_lift_a2() {
+        let plus = |x: u32, y: u32| x + y;
+        assert_eq!(<Option<u32> as Applicative>::lift_a2(plus, None, Some(1738)), None);
     }
 }
