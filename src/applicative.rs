bdcd5d0419b163f39e3b030dc7dc8c4d80c7fc84
Cameron Dart <cdart@anduril.com>
2022-09-20 06:05:19 +0000

    applicative: free your functions

--- src/applicative.rs
+++ src/applicative.rs
@@ -9,13 +9,19 @@ pub trait Applicative: Functor {
     /// In Haskell speak this translates to,
     /// pure :: (Functor f) => a -> f a
     fn pure(a: Self::Inner) -> Self::Wrapped<Self::Inner>;
-    // Run a computation inside a functor.
-    //
-    // In Haskell speak this translates to,
-    // (<*>) :: (Functor f) => f (a -> b) -> f a -> f b -> f b
-    fn lift_a2<B, F, C>(f: F, a: Self::Wrapped<Self::Inner>, b: Self::Wrapped<B>) -> Self::Wrapped<C>
-    where
-        F: Fn(Self::Inner, B) -> C;
+    /// Run a computation inside a functor.
+    ///
+    /// In Haskell speak this translates to,
+    /// (<*>) :: (Functor f) => f (a -> b) -> f a -> f b
+    fn seq<F: Fn(Self::Inner) -> B, B>(f: F, fa: Self::Wrapped<Self::Inner>) -> Self::Wrapped<B>;
+    /// Lift a binary function into actions
+    ///
+    /// lift_a2 :: (a -> b -> c) -> f a -> f b -> f c
+    fn lift_a2<F: Fn(Self::Inner, B) -> C, B, C>(
+        f: F,
+        fa: Self::Wrapped<Self::Inner>,
+        fb: Self::Wrapped<B>,
+    ) -> Self::Wrapped<C>;
 }
 
 impl<A> Applicative for Option<A> {
@@ -25,10 +31,19 @@ impl<A> Applicative for Option<A> {
     }
 
     #[inline]
-    fn lift_a2<B, F, C>(f: F, fa: Self::Wrapped<Self::Inner>, fb: Self::Wrapped<B>) -> Self::Wrapped<C>
-    where
-         F: Fn(Self::Inner, B) -> C,
-    {
+    fn seq<F: Fn(Self::Inner) -> B, B>(f: F, fa: Self::Wrapped<Self::Inner>) -> Self::Wrapped<B> {
+        match fa {
+            Some(a) => Some(f(a)),
+            _ => None,
+        }
+    }
+
+    #[inline]
+    fn lift_a2<F: Fn(Self::Inner, B) -> C, B, C>(
+        f: F,
+        fa: Self::Wrapped<Self::Inner>,
+        fb: Self::Wrapped<B>,
+    ) -> Self::Wrapped<C> {
         match (fa, fb) {
             (Some(a), Some(b)) => Some(f(a, b)),
             _ => None,
@@ -43,17 +58,25 @@ impl<T, E> Applicative for Result<T, E> {
     }
 
     #[inline]
-    fn lift_a2<B, F, C>(f: F, fa: Self::Wrapped<Self::Inner>, fb: Self::Wrapped<B>) -> Self::Wrapped<C>
-    where
-        F: Fn(Self::Inner, B) -> C,
-    {
+    fn lift_a2<F: Fn(Self::Inner, B) -> C, B, C>(
+        f: F,
+        fa: Self::Wrapped<Self::Inner>,
+        fb: Self::Wrapped<B>,
+    ) -> Self::Wrapped<C> {
         match (fa, fb) {
             (Ok(a), Ok(b)) => Ok(f(a, b)),
             (Err(e), _) | (_, Err(e)) => Err(e),
         }
     }
-}
 
+    #[inline]
+    fn seq<F: Fn(Self::Inner) -> B, B>(f: F, fa: Self::Wrapped<Self::Inner>) -> Self::Wrapped<B> {
+        match fa {
+            Ok(a) => Ok(f(a)),
+            Err(e) => Err(e),
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -67,18 +90,27 @@ mod test {
     #[test]
     fn applicative_option_some_lift_a2() {
         let plus = |x: u32, y: u32| x + y;
-        assert_eq!(<Option<u32> as Applicative>::lift_a2(plus, Some(1730), Some(8)), Some(1738));
+        assert_eq!(
+            <Option<u32> as Applicative>::lift_a2(plus, Some(1730), Some(8)),
+            Some(1738)
+        );
     }
 
     #[test]
     fn applicative_option_none_lift_a2() {
         let plus = |x: u32, y: u32| x + y;
-        assert_eq!(<Option<u32> as Applicative>::lift_a2(plus, None, None), None);
+        assert_eq!(
+            <Option<u32> as Applicative>::lift_a2(plus, None, None),
+            None
+        );
     }
 
     #[test]
     fn applicative_option_some_none_lift_a2() {
         let plus = |x: u32, y: u32| x + y;
-        assert_eq!(<Option<u32> as Applicative>::lift_a2(plus, None, Some(1738)), None);
-    } 
+        assert_eq!(
+            <Option<u32> as Applicative>::lift_a2(plus, None, Some(1738)),
+            None
+        );
+    }
 }
